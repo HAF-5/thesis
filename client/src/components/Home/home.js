1aa94cb9297b43855f70fc6cd9ff4be6c54de86d
HoudaRs <houdarouaissi@gmail.com>
2020-01-24 14:09:49 +0100

    first commit

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -4,7 +4,7 @@ export default class home extends Component {
     render() {
         return (
             <div>
-               <p>home</p> 
+               <p> My home </p> 
             </div>
         )
     }
