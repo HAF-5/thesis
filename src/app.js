b82cdef30bfc11cb168e83b73af2e5f0eef58c70
FoozBarakat <reachfooz@gmail.com>
2020-01-25 14:22:26 +0100

    login/signup

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const userRouter = require('./routers/user');
 const path = require('path');
+const morgan = require('morgan');
 
 const publicPath = path.join(__dirname, '..', 'client', 'build');
 
-
-
 require('./db/db');
-
 const app = express();
 
+// import routes
+const userRouter = require('./routers/user');
+
+// app middleware 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(publicPath));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth");
     next();
