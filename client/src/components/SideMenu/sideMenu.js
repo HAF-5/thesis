6fb60498df44f85f47b24b935d8a3ec4b7dc2b6a
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-28 11:26:41 +0100

    some changes on side menu component

--- client/src/components/SideMenu/sideMenu.js
+++ client/src/components/SideMenu/sideMenu.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setMenuElementDispatcher } from './../../store/actions/sideMenu';
+import {elementsData} from './../../sideMenuElementsData';
 
 class sideMenu extends Component {
 
+    styles(styles) {
+        let style = {};
+        styles.forEach(element => {
+            style[`${element.property}`] = element.value;
+        });
+        return style;
+    }
 
-    render() {
+    createElement(elements) {
+        return elements.map(elt => 
+            React.createElement(
+                elt.element, 
+                {
+                    style: this.styles(elt.style),
+                    className: elt.classList.join(' ')
+                }, 
+                (elt.children.length > 0) ? this.createElement(elt.children) : null,
+                elt.content
+            )
+        )
+    }
+    componentDidMount() {
+        console.log( elementsData )
+        this.props.setMenuElements(elementsData);
+    }
+
+    render() { 
+        console.log(this.props.menuItems)
         return (
             <div>
+                {
+                    this.props.menuItems.map(item => (
+                        <div>
+                            <h3>{item.title}</h3>
+                            {
+                                this.createElement(item.elements)
+                            }
+                            
+                        </div>
+                    ))
+                }
             </div>
         )
     }
 }
 
 const mapStateToProps = (state) => ({
+    menuItems: state.sideMenuElements
 })
 
-const mapDispatchToProps = {
-    
-}
+const mapDispatchToProps = (dispatch) => ({
+    setMenuElements: (payload) => dispatch(setMenuElementDispatcher(payload))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(sideMenu)
