eb3e13b2a11d51d5a482afb1407163081311d80c
FoozBarakat <reachfooz@gmail.com>
2020-01-28 11:19:15 +0100

    auth

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
 import Dashboard from './../components/Dashboard/dashboard';
-import Login from './../components/Login/login';
-import Signup from './../components/Signup/signup';
+import Signin from './../components/auth/Signin';
+import Signup from './../components/auth/Signup';
 import Home from './../components/Home/home';
 
 const AppRouter = () => {
-    return (
+  return (
     <Router>
-        <div>
-          <Switch>
-            <PublicRoute path = '/' component = {Home} exact/>
-            <PublicRoute path = '/signup' component = {Signup} />
-            <PublicRoute path = '/login' component = {Login} />
-            <PublicRoute path = '/home' component = {Home} />
-            <PrivateRoute path = '/dashboard' component = {Dashboard}/>
-            <Route>
-              <div>not found</div>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    )
+      <div>
+        <Switch>
+          <PublicRoute path='/' component={Home} exact />
+          <PublicRoute path='/signup' component={Signup} />
+          <PublicRoute path='/signin' component={Signin} />
+          <PublicRoute path='/home' component={Home} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <Route>
+            <div>not found</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default AppRouter;
