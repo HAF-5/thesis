5b600c09e507d6bf2892b9221397a0001caae562
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-28 17:15:39 +0100

    dashboard component

--- client/src/components/Dashboard/dashboard.js
+++ client/src/components/Dashboard/dashboard.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Header from './../Header/header';
+import './dashboard.css'
 
-export class home extends Component {
+export class Dashboard extends Component {
   
   componentDidMount(){
   }
@@ -12,7 +13,61 @@ export class home extends Component {
     return (
       <div>
         <Header/>
-        <h1>WELCOME TO HAF</h1>
+        <div>
+            <div className="header-box">
+            <div className="header-position">
+            <h1> My Sites</h1>  
+            <p className="font">Select a site to edit, view and open its dashboard</p>
+             </div>
+            <a className="btn btn-outline-primary btn-create" href="#" >Create New Website</ a>
+            </div>   
+            <div className="box"> 
+            <p id="p"> All Sites</p>
+                <div className="flex-container">                
+                    <div class="card container-div">  Website </div> 
+                    <div class="card container-div">  Website </div> 
+                    <div class="card container-div-create ">
+                    <div className="icon-position">
+                    <h4 class="line"><i className="fas fa-chess-queen icons"></i> </h4>
+                    </div>
+                    <p class="div-text-create">Start to Create a New Templete </p>
+                    <p>For Free</p>
+                    <a class="btn btn-default btn-lg btn-create-div" href="#">Create New Website</ a>
+                    </div>   
+                    </div>
+                    </div>
+           <footer className="pt-3 my-md-2 pt-md-4">
+                    <div className="container">
+                    <div className="row">
+                    <div className="col-6 col-md">
+                        <h5>Features</h5>
+                        <ul className="list-unstyled text-small">
+                        <li><a className="text-muted" href="#">Cool stuff</a></li>
+                        <li><a className="text-muted" href="#">Another one</a></li>
+                        <li><a className="text-muted" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-6 col-md">
+                        <h5>Resources</h5>
+                        <ul className="list-unstyled text-small">
+                        <li><a className="text-muted" href="#">Resource</a></li>
+                        <li><a className="text-muted" href="#">Resource name</a></li>
+                        <li><a className="text-muted" href="#">Another resource</a></li>
+
+                        </ul>
+                    </div>
+                    <div className="col-6 col-md">
+                        <h5>About</h5>
+                        <ul className="list-unstyled text-small">
+                        <li><a className="text-muted" href="#">Team</a></li>
+                        <li><a className="text-muted" href="#">Locations</a></li>
+                        <li><a className="text-muted" href="#">Privacy</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    </div>
+                </footer>
+        </div>
       </div>
     )
   }
@@ -24,5 +79,5 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(home)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
 
