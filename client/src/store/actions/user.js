0a13a48db3105894c01e04522b7c9e9aad65b43e
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-29 17:43:49 +0100

    connect somethings togather

--- client/src/store/actions/user.js
+++ client/src/store/actions/user.js
@@ -16,20 +16,20 @@ export const logoutDispatcher = () => ({
     type: LOGOUT
 });
 
-// export const logout = (token) => async dispatch => {
-//     try{
-//         let response = await fetch(`/api/user/me/logout`, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'auth': token
-//             }
-//         });
-//         if(response.status === 200){
-//             dispatch(logoutDispatcher());
-//             localStorage.removeItem('token');
-//         }
-//     } catch(err) {
-//         console.log(err)
-//     }
-// }
+export const logout = (token) => async dispatch => {
+    try{
+        let response = await fetch(`/api/user/me/logout`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth': token
+            }
+        });
+        if(response.status === 200){
+            dispatch(logoutDispatcher());
+            localStorage.removeItem('token');
+        }
+    } catch(err) {
+        console.log(err)
+    }
+}
