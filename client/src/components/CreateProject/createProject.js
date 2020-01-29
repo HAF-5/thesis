df6bcdde4b92dfc3eb49eadbda0a8b90604841d0
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-29 18:32:30 +0100

    create project 5

--- client/src/components/CreateProject/createProject.js
+++ client/src/components/CreateProject/createProject.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import './createProject.css'
 import { Link } from 'react-router-dom';
-
-import Dashboard from '../Dashboard/dashboard'
+import Dashboard from '../Dashboard/dashboard';
+// import '../../../node_modules/font-awesome/css/font-awesome.min.xss'
 
 export default class CreateProject extends Component {
     render() {
@@ -11,13 +11,13 @@ export default class CreateProject extends Component {
                 <div className="left">
                    <h2 className="header-title"> HAF 5 </h2> 
                    <div className="left-text">
-                       Create Your Own Template
+                       Create Your Own Template 
                    </div>
-                   <Link to="Dashboard" className="back">  <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Star
-Back</Link>
+                   <Link to="Dashboard" className="back"><i class="fa fa-long-arrow-left icons"></i>Back</Link>
                 </div>
+               
             <div className="right">
-                right
+                right   
             </div>                
             </div>
         )
