2bc1eaae1e751f52513853584213a24e89593d32
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-30 20:16:06 +0100

    websites functionality

--- client/src/components/Dashboard/dashboard.js
+++ client/src/components/Dashboard/dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import FixedNavbar from '../Navbar/FixedNavbar'
+import FixedNavbar from '../Navbar/FixedNavbar';
 import Header from './../Header/header';
 import './dashboard.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import { selectWebsite } from '../../store/actions/websites';
 
 class Dashboard extends Component {
   
@@ -16,20 +17,27 @@ class Dashboard extends Component {
     return (
       <div>
         <Navbar/>  
-            <div className="box"> 
-              <p id="p"> All Sites</p>
-              <div className="flex-container"> 
-              {
-                this.props.websites.map((website) => <div className="card container-div"> {website.title} </div> )
-              }               
-                <div class="card container-div-create">
-                  <p className="div-text-create">Start to Create a New Templete </p>
-                  <p id="pargraph-card-craete">Start Desgin Your Sites</p>
-                  <Link to ="/createProject" className="btn btn-default btn-lg btn-create-div" href="#">Create New Website</ Link>
-                </div>   
-              </div>
-           </div>
-              <Footer/>
+          <div className="box"> 
+            <p id="p"> All Sites</p>
+            <div className="flex-container"> 
+            {
+              this.props.websites.map((website) => (
+                <div className="card container-div" key={website._id}> 
+                  <Link to = {`/editor/${website.title}`}>
+                    {website.title}
+                  </Link>
+                </div> 
+                )
+              )
+            }               
+              <div className="card container-div-create">
+                <p className="div-text-create">Start to Create a New Templete </p>
+                <p id="pargraph-card-craete">Start Desgin Your Sites</p>
+                <Link to ="/createProject" className ="btn btn-default btn-lg btn-create-div">Create New Website</ Link>
+              </div>   
+            </div>
+          </div>
+        <Footer/>
       </div>
     )
   }
@@ -40,6 +48,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
+  selectWebsite: (payload) => dispatch(selectWebsite(payload))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
