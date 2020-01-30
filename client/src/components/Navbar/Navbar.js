2869c8f4bdd8abe7d0919a3a5bd046f2aec189e6
HoudaRs <houdarouaissi@gmail.com>
2020-01-30 19:40:37 +0100

    navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import './FixedNavbar.css'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
-export default class Navbar extends Component{
+import './FixedNavbar.css';
+
+export default class FixedNavbar extends Component{
     // constructor(props) {
     //     super(props);
     //     this.state = {
@@ -18,16 +19,29 @@ export default class Navbar extends Component{
       
     render(){
         return(
-            <div className="Navar">
-                <nav className="navbar navbar-expand-lg navbar-ligth"  style={{height:"60px"}}>
-                    <h5 className="my-0 mr-md-auto font-weight-normal">H A F 5</h5>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
+            <div className="NavarEdit">
+                <nav className="navbar navbar-expand-lg navbar-ligth navedit">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary dropdown-toggle page-btn" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Pages: HOME 
+                     </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">HOME</a>
+                        <a class="dropdown-item" href="#">About</a>
+                        <a class="dropdown-item" href="#">Offer</a>
+                    </div>
+                </div>
+                    <div className="collapse navbar-collapse" id="navbareditor" >
                     <ul className="navbar-nav ml-auto">
+                        
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Profile</a>
+                        <a className="nav-link" href="#">Save</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" data-toggle="popover" data-placement="bottom" title="see what your site looks like before you publish it" href="#">Preview</a>
+                        </li>
+                        <li className="nav-item"id="publish">
+                        <a className="nav-link" data-toggle="tooltip" data-placement="bottom-center" title="click publish to go with your site live">Publish</a>
                         </li>
                     </ul>
                     </div>
