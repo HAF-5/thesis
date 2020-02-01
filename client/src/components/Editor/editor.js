a339fdeb80ee211bc5a231c8607e7afc61b4bf15
AbdoAlabed <henry_3030@icloud.com>
2020-02-01 07:36:49 -0800

    hi

--- client/src/components/Editor/editor.js
+++ client/src/components/Editor/editor.js
@@ -1,62 +1,63 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import SideMenu from '../SideMenu/sideMenu';
-import FixedNavbar from '../Navbar/FixedNavbar';
-import Navbar from '../Navbar/Navbar';
+import SideMenu from "../SideMenu/sideMenu";
+import FixedNavbar from "../Navbar/FixedNavbar";
+import Navbar from "../Navbar/Navbar";
 
-import { setPages, clearPages } from './../../store/actions/pages';
-import { setElements, clearElements } from './../../store/actions/elements';
+import { setPages, clearPages } from "./../../store/actions/pages";
+import { setElements, clearElements } from "./../../store/actions/elements";
 
-import  './Editor.css';
+import "./Editor.css";
 
 class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  componentDidMount() {
+    this.props.setPages(this.props.match.params.id);
+  }
 
-    constructor(props){
-        super(props);
-        this.myRef = React.createRef();
-    }
-    componentDidMount() {
-        this.props.setPages(this.props.match.params.id);
-    }
+  componentWillUnmount() {
+    this.props.clearElements();
+    this.props.clearPages();
+  }
 
-    componentWillUnmount() {
-        this.props.clearElements();
-        this.props.clearPages();
-    }
-
-    render() {
-        this.props.elements.length === 0 && this.props.pages[0] && this.props.setElements(this.props.pages[0]._id);
-        return (
-            <div>
-                <FixedNavbar/>
-                <Navbar/>
-                <div className="row">
-                    <SideMenu />  
-                    <div ref={this.myRef} className=" col-md-11" id="editor" >
-                        {
-                        this.props.elements.map((element, i) => <div
-                            dangerouslySetInnerHTML={{ __html: element.element }}
-                            contentEditable ='true'
-                        ></div>)
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    // this.props.elements.length === 0 && this.props.pages[0] && this.props.setElements(this.props.pages[0]._id);
+    return (
+      <div>
+        <FixedNavbar />
+        <Navbar />
+        <div className="row">
+          <SideMenu />
+          <div ref={this.myRef} className=" col-md-11" id="editor">
+            {this.props.elements.map((element, i) => (
+              <div
+                dangerouslySetInnerHTML={{ __html: element.element }}
+                contentEditable="true"
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, props) => ({
-    elements: state.elements,
-    website: state.websites.filter(website => website._id === props.match.params.id)[0],
-    pages: state.pages
+  elements: state.elements,
+  website: state.websites.filter(
+    website => website._id === props.match.params.id
+  )[0],
+  pages: state.pages
 });
-const mapDispatchToProps = (dispatch) => ({
-    setPages: (websiteId) => dispatch(setPages(websiteId)),
-    setElements: (pageId) => dispatch(setElements(pageId)),
-    clearElements: () => dispatch(clearElements()),
-    clearPages: () => dispatch(clearPages())
+const mapDispatchToProps = dispatch => ({
+  setPages: websiteId => dispatch(setPages(websiteId)),
+  setElements: pageId => dispatch(setElements(pageId)),
+  clearElements: () => dispatch(clearElements()),
+  clearPages: () => dispatch(clearPages())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor);
