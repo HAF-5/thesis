4c431484c9f6ab21764bc5bd2439035cf7594da7
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-03 13:56:02 +0100

    fix select page action

--- client/src/store/actions/pages.js
+++ client/src/store/actions/pages.js
@@ -1,6 +1,16 @@
 import { SET_PAGES, ADD_PAGE, CLEAR_PAGE, SELECT_PAGE} from './constants';
 import { setElements } from './elements';
 
+export const selectPageDispatcher = (payload) => ({
+    type: SELECT_PAGE,
+    payload
+});
+
+export const selectPage = payload => dispatch => {
+    dispatch(setElements(payload._id));
+    dispatch(selectPageDispatcher(payload));
+}
+
 export const setPagesDispatcher = (payload) => ({
     type: SET_PAGES,
     payload
@@ -12,15 +22,12 @@ export const setPages = websiteId => async dispatch => {
     console.log(data)
     if(response.status == 200){
         dispatch(setPagesDispatcher(data));
-        // dispatch(setElements(data._id));
+        console.log(data)
+        data.length > 0 &&  dispatch(selectPage(data[0]));
+
     }
 }
 
 export const clearPages = () => ({
     type: CLEAR_PAGE
 });
-
-export const selectPage = (payload) => ({
-    type: SELECT_PAGE,
-    payload
-})
