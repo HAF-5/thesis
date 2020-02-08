6a6b066237794bc4757d2dbd00ca0101d861d4de
AbdoAlabed <henry_3030@icloud.com>
2020-02-08 18:22:34 +0100

    'style'

--- client/src/components/Sites/site.js
+++ client/src/components/Sites/site.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
 import moment from 'moment'
+import './site.css'
 class Site extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      opacity: 1,
+      isShown: false
+    }
+  }
+
+
   render() {
     return (
-      <div style={{ margin: '50px', display: 'inline-flex' }}>
+      <div style={{ margin: '50px', display: 'inline-flex', height: '50px' }}>
         <section className='px-1' >
-          <div className='card' style={{ width: '22rem', height: '35rem' }}>
-            <div className='view overlay'>
-              <a href='#'>
-                <img src='https://colorlib.com/wp/wp-content/uploads/sites/2/best-personal-website-builder.jpg'
-                  className='img-fluid' alt='sample' />
-                <div className='mask rgba-white-slight waves-effect waves-light'></div>
+          <div style={{ opacity: this.state.opacity }} >
+            <div className='card ' style={{ width: '22rem', height: '35rem' }} onMouseEnter={() => this.setState({ opacity: 0.1, isShown: true })
+            } onMouseLeave={() => this.setState({ opacity: 1, isShown: false })
+            }>
+              <div className='view overlay'>
+                {this.state.isShown &&
+                  <button className='henry-edit' style={{ opacity: 1 }}>Edit</button>
+                }
+                <a href='#'>
+                  <img src='https://colorlib.com/wp/wp-content/uploads/sites/2/best-personal-website-builder.jpg'
+                    className='img-fluid' alt='sample' />
+                  <div className='mask rgba-white-slight waves-effect waves-light'></div>
+                </a>
+              </div>
+              <a className='btn-floating btn-action ml-auto mr-4 mdb-color lighten-3 waves-effect waves-light'>
+                <i className='fas fa-chevron-right pl-1'></i>
               </a>
-            </div>
-            <a className='btn-floating btn-action ml-auto mr-4 mdb-color lighten-3 waves-effect waves-light'>
-              <i className='fas fa-chevron-right pl-1'></i>
-            </a>
-            {/* card content */}
-            <div className="card-body text-center">
-              {console.log(this.props.website)}
-              <h4 className='card-title'>{this.props.website.title}</h4>
-              <hr />
-              <p className='font-small grey-dark-text mb-0'>
-                {this.props.website.description}
-              </p>
-            </div>
-            {/* card footer */}
-            <div className='mdb-color lighten-3 text-center'>
-              <ul className='list-unstyled list-inline font-small mt-3'>
-                <li className='list-inline-item pr-2 white-text'>
-                  <i className='far fa-clock pr-1'></i>
-                  {moment(this.props.website.createdAt).fromNow()}
-                </li>
-              </ul>
+              {/* card content */}
+              <div className="card-body text-center">
+                {console.log(this.props.website)}
+                <h4 className='card-title'>{this.props.website.title}</h4>
+                <hr />
+                <p className='font-small grey-dark-text mb-0'>
+                  {this.props.website.description}
+                </p>
+              </div>
+              {/* card footer */}
+              <div className='mdb-color lighten-3 text-center'>
+                <ul className='list-unstyled list-inline font-small mt-3'>
+                  <li className='list-inline-item pr-2 white-text'>
+                    <i className='far fa-clock pr-1'></i>
+                    {moment(this.props.website.createdAt).fromNow()}
+                  </li>
+                </ul>
+              </div>
+
             </div>
           </div>
         </section>
-      </div>
+      </div >
     );
   }
 }
