8ed28cf5b8c64e76ea782602746ccf090edab2c9
AbdoAlabed <henry_3030@icloud.com>
2020-02-15 19:07:08 +0200

    textEditor

--- client/src/components/Editor/Navbar/Navbar.js
+++ client/src/components/Editor/Navbar/Navbar.js
@@ -7,6 +7,24 @@ import "./../Navbar/Navbar.css";
 
 class FixedNavbar extends Component {
   render() {
+    const fontSize = [
+      8,
+      9,
+      10,
+      11,
+      12,
+      14,
+      16,
+      18,
+      20,
+      22,
+      24,
+      26,
+      28,
+      36,
+      48,
+      72
+    ];
     return (
       <nav className="editor-navbar navbar navbar-expand-lg navbar-light bg-light">
         <button
@@ -74,15 +92,50 @@ class FixedNavbar extends Component {
               </div>
             </li>
             <li className="editor-nav-item nav-item">
-              <div style={{ position: "relative", left: "365px" }}>
+              <div style={{ position: "relative", left: "300px" }}>
+                {/* ///////////////////////////////////// */}
                 <div className="btn-group mr-2">
-                  <button type="button" className="btn btn btn-css">
-                    <i className="fa fa-font"></i>
-                  </button>
-                  <button type="button" className="btn btn btn-css">
+                  <div className="dropdown">
+                    <button
+                      type="button"
+                      className="btn btn-css dropdown-toggle"
+                      id="dropdownMenuMenu"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <i className="fa fa-font"></i>
+                    </button>
+                    <div
+                      className="dropdown-menu "
+                      aria-labelledby="dropdownMenuMenu"
+                    >
+                      <p className="dropdown-item " type="button">
+                        8
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* //////////////////////////////////////// */}
+
+                <div className="btn-group mr-2">
+                  <select className="element-style">
+                    {fontSize.map(size => {
+                      return (
+                        <option value={size} style={{ fontSize: "16px" }}>
+                          {size}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                {/* ///////////////////////// */}
+
+                <div className="btn-group mr-2">
+                  <button type="button" className="btn btn-css">
                     <i className="fa fa-bold"></i>
                   </button>
-                  <button type="button" className="btn btn btn-css">
+                  <button type="button" className="btn btn-css">
                     <i className="fa fa-italic"></i>
                   </button>
                 </div>
