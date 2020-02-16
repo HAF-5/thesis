5f020487abfa7aa1edf0e9975ff258f0df6f8f39
AbdoAlabed <henry_3030@icloud.com>
2020-02-16 11:31:57 +0200

    text-align text-bold

--- client/src/components/Editor/editor.js
+++ client/src/components/Editor/editor.js
@@ -1,198 +1,214 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { ToastContainer, toast } from 'react-toastify';
-import $ from 'jquery';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import $ from "jquery";
 
-import SideMenu from '../SideMenu/sideMenu';
-import FixedNavbar from '../Navbar/FixedNavbar';
-import Navbar from './Navbar/Navbar';
-import AddPageModal from './../AddPageModal/addPageModal';
+import SideMenu from "../SideMenu/sideMenu";
+import FixedNavbar from "../Navbar/FixedNavbar";
+import Navbar from "./Navbar/Navbar";
+import AddPageModal from "./../AddPageModal/addPageModal";
 
-import { setPages, clearPages, selectPage } from './../../store/actions/pages';
-import { setElements, clearElements, addElement, editElement } from './../../store/actions/elements';
-import { addToLastTenSteps } from './../../store/actions/lastTenSteps';
-import Toolbox from './../Toolbox/toolbox';
+import { setPages, clearPages, selectPage } from "./../../store/actions/pages";
+import {
+  setElements,
+  clearElements,
+  addElement,
+  editElement
+} from "./../../store/actions/elements";
+import { addToLastTenSteps } from "./../../store/actions/lastTenSteps";
+import Toolbox from "./../Toolbox/toolbox";
 
 import "./Editor.css";
 
 class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.editor = React.createRef();
+    this.state = {
+      addPageModalIsOpen: false,
+      selectedElement: null,
+      selectedElementProperties: null
+    };
+  }
+  openAddPageModal = () => {
+    this.setState({ addPageModalIsOpen: true });
+  };
 
-    constructor(props){
-        super(props);
-        this.editor = React.createRef();
-        this.state = {
-            addPageModalIsOpen: false,
-            selectedElement: null,
-            selectedElementProperties: null
-        }
+  closeAddPageModal = () => {
+    this.setState({ addPageModalIsOpen: false });
+  };
+
+  componentDidMount() {
+    this.props.setPages(this.props.match.params.id);
+  }
+
+  handleOnDragOver(e) {
+    e.preventDefault();
+  }
+
+  handleOnDrop(e) {
+    let id = e.dataTransfer.getData("id");
+    let element = document.getElementById(id);
+    let stringHTMLB = element.outerHTML;
+    this.props.addToLastTenSteps({ _id: id, element: stringHTMLB });
+
+    $(`#${id}`) && $(`#${id}`).removeClass("selected");
+    element.style.position = "absolute";
+    element.style.left = e.clientX + "px";
+    element.style.top = e.clientY + "px";
+    let stringHTML = element.outerHTML;
+    this.props.editElement({ _id: id, element: stringHTML });
+  }
+
+  domToString = dom => {
+    return dom.outerHTML;
+  };
+
+  createElement = (e, type) => {
+    let target = e.target.cloneNode(true);
+    target.style.position = "absolute";
+    if (type === "navbar") {
+      target.style.left = "0";
+      target.style.top = "14vh";
+    } else {
+      target.style.left = "50vw";
+      target.style.top = "50vh";
     }
-    openAddPageModal = () => {
-        this.setState({ addPageModalIsOpen: true });
+    let element = {
+      type,
+      element: this.domToString(target)
     };
 
-    closeAddPageModal = () => {
-        this.setState({addPageModalIsOpen: false});
-    }
+    this.props.addElement(element);
+  };
 
-    componentDidMount() {
-        this.props.setPages(this.props.match.params.id);
-    }
+  componentWillUnmount() {
+    this.props.clearElements();
+    this.props.clearPages();
+  }
 
-    handleOnDragOver(e) {
-        e.preventDefault();
-    }
+  render() {
+    return (
+      <div>
+        <AddPageModal
+          addPageModalIsOpen={this.state.addPageModalIsOpen}
+          website={this.props.match.params.id}
+          closeModal={this.closeAddPageModal}
+        />
+        <ToastContainer />
+        <FixedNavbar />
+        <Navbar
+          website={this.props.match.params.id}
+          openAddPageModal={this.openAddPageModal}
+          element={this.state.selectedElement}
+          elementProperties={this.state.selectedElementProperties}
+        />
+        <Toolbox
+          element={this.state.selectedElement}
+          elementProperties={this.state.selectedElementProperties}
+        />
+        <SideMenu createElement={(e, type) => this.createElement(e, type)} />
+        <div
+          id="editor"
+          ref={this.editor}
+          onDragOver={e => this.handleOnDragOver(e)}
+          onDrop={e => this.handleOnDrop(e)}
+        >
+          {this.props.elements.map(element => {
+            element.element = element.element
+              .split(">")
+              .map((val, i) => {
+                if (i === 0) {
+                  val += ` id= "${element._id}"`;
+                }
+                return val;
+              })
+              .join(">");
+            let xmlString = element.element;
+            return (
+              <div
+                className="selector"
+                dangerouslySetInnerHTML={{ __html: xmlString }}
+                draggable
+                key={element._id}
+                onDragStart={e => {
+                  e.dataTransfer.setData("id", element._id);
+                  e.dataTransfer.effectAllowed = "move";
+                }}
+                contentEditable={true}
+                onClick={e => {
+                  let id = e.target.id;
+                  if (this.state.selectedElement) {
+                    $(`#${this.state.selectedElement._id}`).removeClass(
+                      "selected"
+                    );
+                  }
+                  let elmnt = $(`#${element._id}`);
+                  let width = elmnt.width();
+                  let height = elmnt.height();
+                  let color = $(elmnt).css("color");
+                  let paddingTop = $(elmnt).css("padding-top");
+                  let paddingRight = $(elmnt).css("padding-right");
+                  let paddingBottom = $(elmnt).css("padding-bottom");
+                  let paddingLeft = $(elmnt).css("padding-left");
+                  let marginTop = $(elmnt).css("margin-top");
+                  let marginRight = $(elmnt).css("margin-right");
+                  let marginBottom = $(elmnt).css("margin-bottom");
+                  let marginLeft = $(elmnt).css("margin-left");
+                  let positionTop = $(elmnt).css("top");
+                  let positionRight = $(elmnt).css("right");
+                  let positionBottom = $(elmnt).css("bottom");
+                  let positionLeft = $(elmnt).css("left");
 
-    handleOnDrop(e) {
-        let id = e.dataTransfer.getData("id");
-        let element = document.getElementById(id);
-        let stringHTMLB = element.outerHTML;
-        this.props.addToLastTenSteps({ _id: id, element: stringHTMLB });
-
-        $(`#${id}`) && $(`#${id}`).removeClass('selected');
-        element.style.position = "absolute";
-        element.style.left = e.clientX + 'px';
-        element.style.top = e.clientY + 'px';
-        let stringHTML = element.outerHTML;
-        this.props.editElement({ _id: id, element: stringHTML });
-    }
-
-    domToString = (dom) => {
-        return dom.outerHTML
-    }
-
-    createElement = (e, type) => {
-        let target = e.target.cloneNode(true);
-        target.style.position = "absolute" ;
-        if(type === 'navbar'){
-            target.style.left = '0' ;
-            target.style.top = '14vh' ;
-        } else {
-            target.style.left = '50vw' ;
-            target.style.top = '50vh' ;
-        }
-        let element = {
-            type,
-            element: this.domToString(target)
-        };
-        
-        this.props.addElement(element);
-    }
-
-    componentWillUnmount() {
-        this.props.clearElements();
-        this.props.clearPages();
-    }
-
-    render() {
-        return (
-            <div>
-                <AddPageModal 
-                    addPageModalIsOpen= {this.state.addPageModalIsOpen}
-                    website= {this.props.match.params.id}
-                    closeModal= {this.closeAddPageModal}
-                />
-                <ToastContainer/>
-                <FixedNavbar/>
-                <Navbar 
-                    website= {this.props.match.params.id} 
-                    openAddPageModal= {this.openAddPageModal}
-                />
-                <Toolbox
-                    element= {this.state.selectedElement}
-                    elementProperties = {this.state.selectedElementProperties}
-                />
-                <SideMenu createElement={(e, type) => this.createElement(e, type)}/> 
-                <div 
-                    id="editor" 
-                    ref= {this.editor} 
-                    onDragOver= {(e) => this.handleOnDragOver(e)}
-                    onDrop= {(e) => this.handleOnDrop(e)}
-                > 
-                    { 
-                        this.props.elements.map(element => {
-                            element.element = element.element.split('>').map((val, i) => {
-                                if(i === 0){
-                                    val += ` id= "${element._id}"` 
-                                }
-                                return val;
-                            }).join('>');
-                            let xmlString = element.element;
-                            return <div 
-                                className= 'selector'
-                                dangerouslySetInnerHTML={{ __html: xmlString }}
-                                draggable 
-                                key= {element._id}
-                                onDragStart= {(e) => {
-                                    e.dataTransfer.setData("id", element._id);
-                                    e.dataTransfer.effectAllowed = 'move';
-                                }}
-                                contentEditable= {true}
-                                onClick= {(e) => {
-                                    let id = e.target.id;
-                                    if(this.state.selectedElement){
-                                        $(`#${this.state.selectedElement._id}`).removeClass('selected');
-                                    }
-                                    let elmnt = $(`#${element._id}`);
-                                    let width = elmnt.width();
-                                    let height = elmnt.height();
-                                    let color = $(elmnt).css('color');
-                                    let paddingTop = $(elmnt).css('padding-top');
-                                    let paddingRight = $(elmnt).css('padding-right');
-                                    let paddingBottom= $(elmnt).css('padding-bottom');
-                                    let paddingLeft = $(elmnt).css('padding-left');
-                                    let marginTop = $(elmnt).css('margin-top');
-                                    let marginRight = $(elmnt).css('margin-right');
-                                    let marginBottom= $(elmnt).css('margin-bottom');
-                                    let marginLeft = $(elmnt).css('margin-left');
-                                    let positionTop = $(elmnt).css('top');
-                                    let positionRight = $(elmnt).css('right');
-                                    let positionBottom= $(elmnt).css('bottom');
-                                    let positionLeft = $(elmnt).css('left');
-
-                                    this.setState(()=>({
-                                        selectedElement: element, 
-                                        selectedElementProperties: {
-                                            width,
-                                            height,
-                                            paddingTop,
-                                            paddingRight,
-                                            paddingBottom,
-                                            paddingLeft,
-                                            marginTop,
-                                            marginRight,
-                                            marginBottom,
-                                            marginLeft,
-                                            positionTop,
-                                            positionRight,
-                                            positionBottom,
-                                            positionLeft
-                                    }}), () => {});
-                                }}
-                            />
-                        })  
-                    }
-                    {
-                        !this.props.pages.length && 
-                        <div className= "editor_inform-page-section">
-                            <h3>There is no page in your website.</h3>
-                            <button 
-                                className= "btn editor_add-page-section_button"
-                                onClick= {this.openAddPageModal}
-                            >
-                                <i class="fas fa-plus"></i> Add Page
-                            </button>
-                        </div>
-                    }
-                    {
-                        !this.props.elements.length && 
-                        this.props.pages.length &&
-                        <div className= "editor_inform-page-section">
-                            <h3>{this.props.selectedPage.title} page is empty, create some elements.</h3>
-                        </div>
-                    }
-                </div>
+                  this.setState(
+                    () => ({
+                      selectedElement: element,
+                      selectedElementProperties: {
+                        width,
+                        height,
+                        paddingTop,
+                        paddingRight,
+                        paddingBottom,
+                        paddingLeft,
+                        marginTop,
+                        marginRight,
+                        marginBottom,
+                        marginLeft,
+                        positionTop,
+                        positionRight,
+                        positionBottom,
+                        positionLeft
+                      }
+                    }),
+                    () => {}
+                  );
+                }}
+              />
+            );
+          })}
+          {!this.props.pages.length && (
+            <div className="editor_inform-page-section">
+              <h3>There is no page in your website.</h3>
+              <button
+                className="btn editor_add-page-section_button"
+                onClick={this.openAddPageModal}
+              >
+                <i class="fas fa-plus"></i> Add Page
+              </button>
             </div>
           )}
+          {!this.props.elements.length && this.props.pages.length && (
+            <div className="editor_inform-page-section">
+              <h3>
+                {this.props.selectedPage.title} page is empty, create some
+                elements.
+              </h3>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, props) => ({
@@ -204,15 +220,15 @@ const mapStateToProps = (state, props) => ({
   selectedPage: state.selectedPage
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    setPages: (websiteId) => dispatch(setPages(websiteId)),
-    setElements: (pageId) => dispatch(setElements(pageId)),
-    clearElements: () => dispatch(clearElements()),
-    clearPages: () => dispatch(clearPages()),
-    selectPage: () => dispatch(selectPage()),
-    addElement: (page) => dispatch(addElement(page)),
-    editElement: (element) => dispatch(editElement(element)),
-    addToLastTenSteps: (element) => dispatch(addToLastTenSteps(element))
+const mapDispatchToProps = dispatch => ({
+  setPages: websiteId => dispatch(setPages(websiteId)),
+  setElements: pageId => dispatch(setElements(pageId)),
+  clearElements: () => dispatch(clearElements()),
+  clearPages: () => dispatch(clearPages()),
+  selectPage: () => dispatch(selectPage()),
+  addElement: page => dispatch(addElement(page)),
+  editElement: element => dispatch(editElement(element)),
+  addToLastTenSteps: element => dispatch(addToLastTenSteps(element))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor);
